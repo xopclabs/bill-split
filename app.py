244a4c2cc305207dec4ae61784b6d9a07090e1d5
xopclabs <xopclabs@gmail.com>
2021-06-22 03:10:42 +0300

    Working table manipulation

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ class SplitterUI:
         self.people_textbox.add_key_command(py_cui.keys.KEY_ENTER, self.people_add_person)
         self.people_scrollmenu.add_key_command(py_cui.keys.KEY_DELETE, self.people_del_person)
         self.people_scrollmenu.add_key_command(py_cui.keys.KEY_D_LOWER, self.people_del_person)
+        # Add cursor line
+        self.people_scrollmenu.set_selected_color(py_cui.WHITE_ON_GREEN)
 
     def people_add_person(self):
         person = str(self.people_textbox.get()).strip()
@@ -31,27 +33,45 @@ class SplitterUI:
         # Get names
         names = self.people_scrollmenu.get_item_list()
         self.splitter = Splitter(names)
+
         # Create new widget set for next screen
         self.cart_set = self.root.create_new_widget_set(7, 3 + len(names))
+
         # Add labels for each name
         self.cart_names_labels = {}
         for i, name in enumerate(self.splitter.names):
             self.cart_names_labels[name] = self.cart_set.add_label(name, 0, 3 + i)
-        # Add button for entering items
-        self.cart_items_button = self.cart_set.add_button('Add item', 0, 0, column_span=2, command=self.cart_add_item)
+
         # Add scrollmenus for items, prices and quantities
         self.cart_scrollmenus = {}
         self.cart_scrollmenus['item'] = self.cart_set.add_scroll_menu('Item', 1, 0, row_span=5, column_span=2)
         self.cart_scrollmenus['price'] = self.cart_set.add_scroll_menu('Price', 1, 2, row_span=5, column_span=1)
         for i, name in enumerate(self.splitter.names):
             self.cart_scrollmenus[name] = self.cart_set.add_scroll_menu(name, 1, 3 + i, row_span=5, column_span=1)
+        # General settings that apply to every scrollmenu
         for menu in self.cart_scrollmenus.values():
+            # Hotkeys
+            menu.add_key_command(py_cui.keys.KEY_A_LOWER, self.cart_add_item)
+
             menu.add_key_command(py_cui.keys.KEY_DELETE, self.cart_del_item)
             menu.add_key_command(py_cui.keys.KEY_D_LOWER, self.cart_del_item)
+
             menu.add_key_command(py_cui.keys.KEY_J_LOWER, self.cart_scroll_down)
+            menu.add_key_command(py_cui.keys.KEY_DOWN_ARROW, self.cart_scroll_down)
+
             menu.add_key_command(py_cui.keys.KEY_K_LOWER, self.cart_scroll_up)
+            menu.add_key_command(py_cui.keys.KEY_UP_ARROW, self.cart_scroll_up)
+            # Coloring
+            menu.set_selected_color(py_cui.WHITE_ON_GREEN)
+        # Specific hotkeys
+        self.cart_scrollmenus['item'].add_key_command(py_cui.keys.KEY_C_LOWER, self.cart_change_item)
+        self.cart_scrollmenus['price'].add_key_command(py_cui.keys.KEY_C_LOWER, self.cart_change_price)
+        for name in self.splitter.names:
+            menu.add_key_command(py_cui.keys.KEY_C_LOWER, lambda: self.cart_change_quantity(name))
+
         # Change title
         self.root.set_title('bill-split | Shopping cart')
+
         # Change screen to shopping cart editing screen
         self.root.apply_widget_set(self.cart_set)
 
@@ -62,7 +82,7 @@ class SplitterUI:
             for name in self.splitter.names:
                 self.cart_scrollmenus[name].add_item(str(0))
 
-        self.root.show_text_box_popup('Enter new item name (and optionally, price and people buying it)', add_item)
+        self.root.show_text_box_popup('Enter item name (and optionally, price and people buying it)', add_item)
 
     def cart_del_item(self):
         for menu in self.cart_scrollmenus.values():
@@ -76,6 +96,20 @@ class SplitterUI:
         for menu in self.cart_scrollmenus.values():
             viewport_height = menu.get_viewport_height()
             menu._scroll_down(viewport_height)
+
+    def _change_entry(self, key, text):
+        menu = self.cart_scrollmenus[key]
+        menu.set_selected_item(str(text).strip())
+
+    def cart_change_item(self):
+        self.root.show_text_box_popup('Change item name to', lambda text: self._change_entry('item', text))
+
+    def cart_change_price(self):
+        self.root.show_text_box_popup('Change item price to', lambda text: self._change_entry('price', text))
+
+    def cart_change_quantity(self, name):
+        self.root.show_text_box_popup(f'Change quantity for {name} to', lambda text: self._change_entry(name, text))
+
 
 if __name__ == '__main__':
     root = py_cui.PyCUI(5, 20)
